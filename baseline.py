32c808dc5f319cfa8111923a69bb2ee5ae39d570
jaideepwhabi <jwhabi@hawk.iit.edu>
2019-08-30 15:31:10 -0500

    undersampling

--- baseline.py
+++ baseline.py
@@ -11,8 +11,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 import xgboost as xgb
-from sklearn.metrics import roc_auc_score
-
+from sklearn.metrics import roc_auc_score,recall_score,precision_score,accuracy_score,confusion_matrix
+#from imblearn.under_sampling import RandomUnderSampler
+import random
 
 df1=pd.read_csv("C:\\Users\\jaideep.whabi\\ieee-fraud-detection\\train_transaction.csv")
 
@@ -108,7 +109,7 @@ print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
 #ROC AUC 0.8979520526895398 - 350 rounds
 
 #without pca
-#ROC AUC 0.8588492325357053 - 10 rounds
+#ROC AUC 0.8597127557858613 - 10 rounds
 
 #without pca and without imputation run TO-DO: code cleanup if this works
 feature_importance=clf.get_score(importance_type='gain')
@@ -141,4 +142,80 @@ y_pred=clf.predict(dtest)
 
 print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
 #only important features as per all first model
-#ROC AUC 0.853398695139962 - 10 rounds
+#ROC AUC 0.8522085343053979 - 10 rounds
+pred=y_pred
+pred=np.where(pred>0.5,1,0)
+accuracy = pd.DataFrame(columns=['Accuracy'])
+
+acu=accuracy_score(test_Y, pred)
+rec=recall_score(test_Y, pred,average=None)
+pre=precision_score(test_Y, pred,average=None)
+accuracy=accuracy.append([{'Accuracy':acu,'Recall Score': rec, 'Precision Score': pre}])
+for c in accuracy.columns:
+    print(accuracy[c])
+"""
+0    0.975558
+Name: Accuracy, dtype: float64
+0    [0.976529314358396, 0.8965853658536586]
+Name: Precision Score, dtype: object
+0    [0.998699194973493, 0.31965217391304346]
+Name: Recall Score, dtype: object
+"""
+#rus = RandomUnderSampler(random_state=42)
+#X_res, y_res = rus.fit_resample(train_X, train_Y)
+
+indexes=list(data1[data1.isFraud==1].index)
+
+
+i2=random.sample(list(data1[data1.isFraud==0].index), len(indexes))
+
+indexes = indexes + i2
+
+data1=data1.loc[indexes]
+train_X, test_X, train_Y, test_Y = train_test_split( data1.drop('isFraud',axis=1), data1['isFraud'], test_size=1/7.0, random_state=0)
+
+
+params = {'n_estimators':500,
+    'max_depth':9,
+    'learning_rate':0.05,
+    'subsample':0.9,
+    'colsample_bytree':0.9,
+    "eval_metric": "auc"}
+val_X=train_X[:1000]
+partial_train_X=train_X[1000:]
+val_Y=train_Y[:1000]
+partial_train_Y=train_Y[1000:]
+
+dtrain=xgb.DMatrix(partial_train_X,partial_train_Y)
+dval=xgb.DMatrix(val_X,val_Y)
+dtest=xgb.DMatrix(test_X)
+clf=xgb.train(params,dtrain,num_boost_round=1000,early_stopping_rounds=50, evals=[(dtrain,'train'),(dval,'test')],verbose_eval=True)
+
+y_pred=clf.predict(dtest)
+#y_pred=clf.predict_proba(dtest)
+
+print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
+#only important features + undersampling as per all first model
+#ROC AUC 0.8669848039836963 - 10 rounds
+#ROC AUC 0.91034886502859 - 300 rounds
+#ROC AUC 0.9132674006601533 - 500 rounds
+#ROC AUC 0.9136013001934172 - 1000 rounds
+pred=y_pred
+pred=np.where(pred>0.5,1,0)
+accuracy = pd.DataFrame(columns=['Accuracy'])
+
+acu=accuracy_score(test_Y, pred)
+rec=recall_score(test_Y, pred,average=None)
+pre=precision_score(test_Y, pred,average=None)
+accuracy=accuracy.append([{'Accuracy':acu,'Recall Score': rec, 'Precision Score': pre}])
+for c in accuracy.columns:
+    print(accuracy[c])
+"""
+1000 rounds
+0    0.844343
+Name: Accuracy, dtype: float64
+0    [0.8236931642437365, 0.8693373268438787]
+Name: Precision Score, dtype: object
+0    [0.8841301460823373, 0.8029045643153527]
+Name: Recall Score, dtype: object
+"""
