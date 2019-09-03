1b52620a6f9ec159b8572d64acc39cb17af15a86
jaideepwhabi <jwhabi@hawk.iit.edu>
2019-09-03 16:36:46 -0500

    correcting undersampling approach

--- baseline.py
+++ baseline.py
@@ -23,18 +23,6 @@ data=pd.merge(df1,df2,how='left',on='TransactionID')
 
 del df1,df2
 
-data.dtypes
-
-data.head()
-
-len(data.TransactionID.unique()) == len(data)
-
-data.card2.unique()
-data.card2.dtype
-data.card4=data.card4.replace(np.nan,0)
-
-data.card4=data.card4.astype('category')
-data.card4=data.card4.cat.codes
 
 for i in data.columns:
     #data[i]=data[i].replace(np.nan,0) # uncomment if using pca
@@ -44,10 +32,25 @@ for i in data.columns:
         data[i]=data[i].astype('category')
         data[i]=data[i].cat.codes
         
-data.isnull().values.any()        
+#data.isnull().values.any()        
 data=data.drop('TransactionID',axis=1)
 
+
+
 train_X, test_X, train_Y, test_Y = train_test_split( data.drop('isFraud',axis=1), data['isFraud'], test_size=1/7.0, random_state=0)
+train_Y.value_counts()
+#changing: undersampling to happen before feature selection
+indexes=list(train_Y[train_Y==1].index)
+
+i2=random.sample(list(train_Y[train_Y==0].index), len(indexes))
+
+indexes = indexes + i2
+
+train_X=train_X.loc[indexes]
+train_Y=train_Y.loc[indexes]
+
+train_Y.hist()
+
 
 #X=data.drop('isFraud',axis=1)
 #Y=data['isFraud']
@@ -82,7 +85,7 @@ clf = xgb.XGBClassifier(
 )
 
 clf.fit(train_X, train_Y)
-"""
+
 params = {'n_estimators':500,
     'max_depth':9,
     'learning_rate':0.05,
@@ -154,6 +157,7 @@ accuracy=accuracy.append([{'Accuracy':acu,'Recall Score': rec, 'Precision Score'
 for c in accuracy.columns:
     print(accuracy[c])
 """
+"""
 0    0.975558
 Name: Accuracy, dtype: float64
 0    [0.976529314358396, 0.8965853658536586]
@@ -161,17 +165,11 @@ Name: Precision Score, dtype: object
 0    [0.998699194973493, 0.31965217391304346]
 Name: Recall Score, dtype: object
 """
+"""
 #rus = RandomUnderSampler(random_state=42)
 #X_res, y_res = rus.fit_resample(train_X, train_Y)
 
-indexes=list(data1[data1.isFraud==1].index)
 
-
-i2=random.sample(list(data1[data1.isFraud==0].index), len(indexes))
-
-indexes = indexes + i2
-
-data1=data1.loc[indexes]
 train_X, test_X, train_Y, test_Y = train_test_split( data1.drop('isFraud',axis=1), data1['isFraud'], test_size=1/7.0, random_state=0)
 
 
@@ -211,11 +209,87 @@ accuracy=accuracy.append([{'Accuracy':acu,'Recall Score': rec, 'Precision Score'
 for c in accuracy.columns:
     print(accuracy[c])
 """
-1000 rounds
+"""
+1000 rounds (undersampling after feature selection)
 0    0.844343
 Name: Accuracy, dtype: float64
 0    [0.8236931642437365, 0.8693373268438787]
 Name: Precision Score, dtype: object
 0    [0.8841301460823373, 0.8029045643153527]
+Name: Recall Score, dtype: object
+"""
+
+params = {'n_estimators':500,
+    'max_depth':9,
+    'learning_rate':0.05,
+    'subsample':0.9,
+    'colsample_bytree':0.9,
+    "eval_metric": "auc"}
+val_i=random.sample(list(train_Y.index), 5000)
+val_X=train_X.loc[val_i]
+partial_train_X=train_X.loc[val_i]
+val_Y=train_Y.loc[val_i]
+partial_train_Y=train_Y.loc[val_i]
+val_Y.hist()
+partial_train_Y.value_counts()
+
+dtrain=xgb.DMatrix(partial_train_X,partial_train_Y)
+dval=xgb.DMatrix(val_X,val_Y)
+dtest=xgb.DMatrix(test_X)
+clf=xgb.train(params,dtrain,num_boost_round=300,early_stopping_rounds=50, evals=[(dtrain,'train'),(dval,'test')],verbose_eval=True)
+
+y_pred=clf.predict(dtest)
+#y_pred=clf.predict_proba(dtest)
+
+print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
+#ROC AUC 0.9010603919275391 - 300 rounds
+
+feature_importance=clf.get_score(importance_type='gain')
+feature_importance=pd.DataFrame(data={'feature':list(feature_importance.keys()),'importance':list(feature_importance.values())})
+feature_importance.sort_values(by='importance',ascending=False)
+filters=feature_importance[feature_importance.importance>0.8]['feature']
+filters.append(pd.Series('isFraud'))
+data1=data[filters.append(pd.Series('isFraud'))]
+train_X, test_X, train_Y, test_Y = train_test_split( data1.drop('isFraud',axis=1), data1['isFraud'], test_size=1/7.0, random_state=0)
+
+
+params = {'n_estimators':500,
+    'max_depth':9,
+    'learning_rate':0.05,
+    'subsample':0.9,
+    'colsample_bytree':0.9,
+    "eval_metric": "auc"}
+val_X=train_X[:5000]
+partial_train_X=train_X[5000:]
+val_Y=train_Y[:5000]
+partial_train_Y=train_Y[5000:]
+
+dtrain=xgb.DMatrix(partial_train_X,partial_train_Y)
+dval=xgb.DMatrix(val_X,val_Y)
+dtest=xgb.DMatrix(test_X)
+clf2=xgb.train(params,dtrain,num_boost_round=300,early_stopping_rounds=50, evals=[(dtrain,'train'),(dval,'test')],verbose_eval=True)
+
+y_pred=clf2.predict(dtest)
+#y_pred=clf.predict_proba(dtest)
+
+print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
+#only important features as per all first model
+#ROC AUC 0.9201510578249473 - 300 rounds
+pred=y_pred
+pred=np.where(pred>0.5,1,0)
+accuracy = pd.DataFrame(columns=['Accuracy'])
+
+acu=accuracy_score(test_Y, pred)
+rec=recall_score(test_Y, pred,average=None)
+pre=precision_score(test_Y, pred,average=None)
+accuracy=accuracy.append([{'Accuracy':acu,'Recall Score': rec, 'Precision Score': pre}])
+for c in accuracy.columns:
+    print(accuracy[c])
+""" 300 rounds... first undersampling
+0    0.848747
+Name: Accuracy, dtype: float64
+0    [0.8392571245597182, 0.8594030924128011]
+Name: Precision Score, dtype: object
+0    [0.8701859229747676, 0.826417704011065]
 Name: Recall Score, dtype: object
 """
