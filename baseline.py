cea943078f24c5a0aa7d3ecea90d9a8d0a456482
jaideepwhabi <jwhabi@hawk.iit.edu>
2019-09-05 17:26:31 -0500

    tried lgbm basic

--- baseline.py
+++ baseline.py
@@ -95,7 +95,7 @@ partial_train_Y.value_counts()
 dtrain=xgb.DMatrix(partial_train_X,partial_train_Y)
 dval=xgb.DMatrix(val_X,val_Y)
 dtest=xgb.DMatrix(test_X)
-clf=xgb.train(params,dtrain,num_boost_round=600,early_stopping_rounds=50, evals=[(dtrain,'train'),(dval,'test')],verbose_eval=True)
+clf=xgb.train(params,dtrain,num_boost_round=300,early_stopping_rounds=50, evals=[(dtrain,'train'),(dval,'test')],verbose_eval=True)
 
 y_pred=clf.predict(dtest)
 #y_pred=clf.predict_proba(dtest)
@@ -108,7 +108,7 @@ print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
 #ROC AUC 0.9508173314349682 - undersample after split and feature scaling
 
 #ROC AUC 0.9600422200727456 - undersample after split 600 iterations
-
+"""
 feature_importance=clf.get_score(importance_type='gain')
 feature_importance=pd.DataFrame(data={'feature':list(feature_importance.keys()),'importance':list(feature_importance.values())})
 feature_importance.sort_values(by='importance',ascending=False)
@@ -150,9 +150,34 @@ pre=precision_score(test_Y, pred,average=None)
 accuracy=accuracy.append([{'Accuracy':acu,'Recall Score': rec, 'Precision Score': pre}])
 for c in accuracy.columns:
     print(accuracy[c])
+"""
+#trying lightgbm
+    
+import lightgbm as lgb
 
+params={'learning_rate': 0.01,
+        'objective': 'binary',
+        'metric': 'auc',
+        'num_threads': -1,
+        'num_leaves': 256,
+        'verbose': 1,
+        'random_state': 42,
+        'bagging_fraction': 1,
+        'feature_fraction': 0.85
+       }
 
+lgb_train = lgb.Dataset(partial_train_X,partial_train_Y)
+lgb_eval = lgb.Dataset(val_X,val_Y, reference=lgb_train)
+lgb_test = lgb.Dataset(test_X)
+clf = lgb.train(params,
+                lgb_train,
+                num_boost_round=300,
+                valid_sets=lgb_eval,
+                early_stopping_rounds=20)
 
+y_pred=clf.predict(test_X)
+print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
+#ROC AUC 0.9397014160949145 - 300 rounds lgbm
 
 """Test data read and predict"""
 
@@ -176,12 +201,12 @@ s_data=data.drop('TransactionID',axis=1)
 #s_data -= mean
 #s_data /= std
 
-dtest=xgb.DMatrix(s_data)
+#dtest=xgb.DMatrix(s_data)
 
-y_pred=clf.predict(dtest)
+y_pred=clf.predict(s_data)
 
 submit= pd.DataFrame(data={'TransactionID':data['TransactionID'],'isFraud':y_pred})
-submit.to_csv("C:\\Users\\jaideep.whabi\\ieee-fraud-detection\\submission5.csv",index=False)
+submit.to_csv("C:\\Users\\jaideep.whabi\\ieee-fraud-detection\\submission7.csv",index=False)
 
 #submission 1 is undersampling prior to split with split ratio as 1/7.0... 
 #submission 2 is undersampling post split with split ratio as 0.05
@@ -190,171 +215,7 @@ submit.to_csv("C:\\Users\\jaideep.whabi\\ieee-fraud-detection\\submission5.csv",
 #only 2000 rows.... and only a slightly better test result...check kaggle submission
 #submission 4 is undersampling after split with feature scaling
 #submission 5 is undersampling after split while training with 600 iterations.
+#submission6 same as submission2 but 300 iterations of xgboost
+#submission7 is lgbm 300 iterations
 ###################################################################
 
-#X=data.drop('isFraud',axis=1)
-#Y=data['isFraud']
-"""
-scaler = StandardScaler()
-# Fit on training set only.
-scaler.fit(train_X)
-# Apply transform to both the training set and the test set.
-train_X = scaler.transform(train_X)
-test_X = scaler.transform(test_X)
-
-pca = PCA(.95)
-pca.fit(train_X)
-pca.n_components_
-
-#pca = PCA(n_components=10)
-#principalComponents = pca.fit_transform(X)
-#principalDf = pd.DataFrame(data = principalComponents)
-train_X = pca.transform(train_X)
-test_X = pca.transform(test_X)
-
-"""
-#principalDf.head()
-""" DONOT USE
-clf = xgb.XGBClassifier(
-    n_estimators=500,
-    max_depth=9,
-    learning_rate=0.05,
-    subsample=0.9,
-    colsample_bytree=0.9,
-#    tree_method='gpu_hist'
-)
-
-clf.fit(train_X, train_Y)
-
-params = {'n_estimators':500,
-    'max_depth':9,
-    'learning_rate':0.05,
-    'subsample':0.9,
-    'colsample_bytree':0.9,
-    "eval_metric": "auc"}
-val_X=train_X[:10000]
-partial_train_X=train_X[10000:]
-val_Y=train_Y[:10000]
-partial_train_Y=train_Y[10000:]
-
-dtrain=xgb.DMatrix(partial_train_X,partial_train_Y)
-dval=xgb.DMatrix(val_X,val_Y)
-dtest=xgb.DMatrix(test_X)
-clf=xgb.train(params,dtrain,num_boost_round=10,early_stopping_rounds=50, evals=[(dtrain,'train'),(dval,'test')],verbose_eval=True)
-
-y_pred=clf.predict(dtest)
-#y_pred=clf.predict_proba(dtest)
-
-print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
-#ROC AUC 0.8420033677938177 - 10 rounds
-#ROC AUC 0.8606789199156557 - 50 rounds
-#ROC AUC 0.8889456436370466 - 200 rounds
-#ROC AUC 0.8979520526895398 - 350 rounds
-
-#without pca
-#ROC AUC 0.8597127557858613 - 10 rounds
-
-#without pca and without imputation run TO-DO: code cleanup if this works
-feature_importance=clf.get_score(importance_type='gain')
-feature_importance=pd.DataFrame(data={'feature':list(feature_importance.keys()),'importance':list(feature_importance.values())})
-feature_importance.sort_values(by='importance',ascending=False)
-filters=feature_importance[feature_importance.importance>10]['feature']
-filters.append(pd.Series('isFraud'))
-data1=data[filters.append(pd.Series('isFraud'))]
-train_X, test_X, train_Y, test_Y = train_test_split( data1.drop('isFraud',axis=1), data1['isFraud'], test_size=1/7.0, random_state=0)
-
-
-params = {'n_estimators':500,
-    'max_depth':9,
-    'learning_rate':0.05,
-    'subsample':0.9,
-    'colsample_bytree':0.9,
-    "eval_metric": "auc"}
-val_X=train_X[:10000]
-partial_train_X=train_X[10000:]
-val_Y=train_Y[:10000]
-partial_train_Y=train_Y[10000:]
-
-dtrain=xgb.DMatrix(partial_train_X,partial_train_Y)
-dval=xgb.DMatrix(val_X,val_Y)
-dtest=xgb.DMatrix(test_X)
-clf=xgb.train(params,dtrain,num_boost_round=10,early_stopping_rounds=50, evals=[(dtrain,'train'),(dval,'test')],verbose_eval=True)
-
-y_pred=clf.predict(dtest)
-#y_pred=clf.predict_proba(dtest)
-
-print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
-#only important features as per all first model
-#ROC AUC 0.8522085343053979 - 10 rounds
-pred=y_pred
-pred=np.where(pred>0.5,1,0)
-accuracy = pd.DataFrame(columns=['Accuracy'])
-
-acu=accuracy_score(test_Y, pred)
-rec=recall_score(test_Y, pred,average=None)
-pre=precision_score(test_Y, pred,average=None)
-accuracy=accuracy.append([{'Accuracy':acu,'Recall Score': rec, 'Precision Score': pre}])
-for c in accuracy.columns:
-    print(accuracy[c])
-"""
-"""
-0    0.975558
-Name: Accuracy, dtype: float64
-0    [0.976529314358396, 0.8965853658536586]
-Name: Precision Score, dtype: object
-0    [0.998699194973493, 0.31965217391304346]
-Name: Recall Score, dtype: object
-"""
-"""
-#rus = RandomUnderSampler(random_state=42)
-#X_res, y_res = rus.fit_resample(train_X, train_Y)
-
-
-train_X, test_X, train_Y, test_Y = train_test_split( data1.drop('isFraud',axis=1), data1['isFraud'], test_size=1/7.0, random_state=0)
-
-
-params = {'n_estimators':500,
-    'max_depth':9,
-    'learning_rate':0.05,
-    'subsample':0.9,
-    'colsample_bytree':0.9,
-    "eval_metric": "auc"}
-val_X=train_X[:1000]
-partial_train_X=train_X[1000:]
-val_Y=train_Y[:1000]
-partial_train_Y=train_Y[1000:]
-
-dtrain=xgb.DMatrix(partial_train_X,partial_train_Y)
-dval=xgb.DMatrix(val_X,val_Y)
-dtest=xgb.DMatrix(test_X)
-clf=xgb.train(params,dtrain,num_boost_round=1000,early_stopping_rounds=50, evals=[(dtrain,'train'),(dval,'test')],verbose_eval=True)
-
-y_pred=clf.predict(dtest)
-#y_pred=clf.predict_proba(dtest)
-
-print('ROC AUC {}'.format(roc_auc_score(test_Y, y_pred)))
-#only important features + undersampling as per all first model
-#ROC AUC 0.8669848039836963 - 10 rounds
-#ROC AUC 0.91034886502859 - 300 rounds
-#ROC AUC 0.9132674006601533 - 500 rounds
-#ROC AUC 0.9136013001934172 - 1000 rounds
-pred=y_pred
-pred=np.where(pred>0.5,1,0)
-accuracy = pd.DataFrame(columns=['Accuracy'])
-
-acu=accuracy_score(test_Y, pred)
-rec=recall_score(test_Y, pred,average=None)
-pre=precision_score(test_Y, pred,average=None)
-accuracy=accuracy.append([{'Accuracy':acu,'Recall Score': rec, 'Precision Score': pre}])
-for c in accuracy.columns:
-    print(accuracy[c])
-"""
-"""
-1000 rounds (undersampling after feature selection)
-0    0.844343
-Name: Accuracy, dtype: float64
-0    [0.8236931642437365, 0.8693373268438787]
-Name: Precision Score, dtype: object
-0    [0.8841301460823373, 0.8029045643153527]
-Name: Recall Score, dtype: object
-"""
